7c87b29e3c1f6e776b2488389bd65660cf082521
chaudhuree <chaudhuree00@gmail.com>
2023-09-21 21:24:12 +0600

    tailwind @apply method useage

--- job-search/src/components/ActionButton.vue
+++ job-search/src/components/ActionButton.vue
@@ -1,7 +1,5 @@
 <template>
-  <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
-  >
+  <button class="primary">
     {{ text }}
   </button>
 </template>
@@ -12,3 +10,15 @@ export default {
   props: ['text']
 }
 </script>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium;
+}
+.primary {
+  @apply bg-brand-blue-1 text-white hover:shadow-blue;
+}
+.secondary {
+  @apply bg-white text-brand-blue-1 border border-brand-blue-1 hover:shadow-blue;
+}
+</style>
