31f64d02caa52ccab40508858594aad11c8aaba7
chaudhuree <chaudhuree00@gmail.com>
2023-09-25 19:51:29 +0600

    job search form created and implemented in TheHero section

--- job-search/src/components/TheHero.vue
+++ job-search/src/components/TheHero.vue
@@ -1,14 +1,20 @@
 <template>
   <main>
-    <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20">
+    <section class="flex h-screen flex-col pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"></div>
 
         <div class="col-span-5 col-start-2">
           <the-headline />
+          <job-search-form />
         </div>
 
-        <div class="col-span-5 col-start-7"></div>
+        <div class="col-span-5 col-start-7 self-center justify-self-center">
+          <img
+            class="h-80 w-80 object-contain"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
+          />
+        </div>
 
         <div class="col-span-1 col-start-12"></div>
       </div>
@@ -18,11 +24,13 @@
 
 <script>
 import TheHeadline from '@/components/TheHeadline.vue'
+import JobSearchForm from './JobSearchForm.vue'
 
 export default {
   name: 'TheHero',
   components: {
-    TheHeadline
+    TheHeadline,
+    JobSearchForm
   }
 }
 </script>
