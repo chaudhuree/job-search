57f1d72f927a249493763dd2e7ddee18c7e7b92c
chaudhuree <chaudhuree00@gmail.com>
2023-09-30 05:43:11 +0600

    coded MainNav for dynamic nav text and url

--- job-search/src/components/Navigation/MainNav.vue
+++ job-search/src/components/Navigation/MainNav.vue
@@ -1,25 +1,17 @@
 <template>
   <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
-      <div
-        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
-      >
-        <router-link
-          :to="{ name: 'Home' }"
-          class="flex h-full items-center text-xl"
+      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
+        <router-link :to="{ name: 'Home' }" class="flex h-full items-center text-xl"
           >Job Jam
         </router-link>
 
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
-            <li
-              v-for="menuItem in menuItems"
-              :key="menuItem"
-              class="ml-9 h-full first:ml-0"
-            >
-              <a href="" class="flex h-full items-center py-2.5">{{
-                menuItem
-              }}</a>
+            <li v-for="menuItem in menuItems" :key="menuItem.text" class="ml-9 h-full first:ml-0">
+              <router-link :to="menuItem.url" class="flex h-full items-center py-2.5">{{
+                menuItem.text
+              }}</router-link>
             </li>
           </ul>
         </nav>
@@ -36,42 +28,42 @@
 </template>
 
 <script>
-import ActionButton from "@/components/Shared/ActionButton.vue";
-import ProfileImage from "@/components/Navigation/ProfileImage.vue";
-import TheSubnav from "@/components/Navigation/TheSubnav.vue";
+import ProfileImage from '@/components/Navigation/ProfileImage.vue'
+import TheSubnav from '@/components/Navigation/TheSubnav.vue'
+import ActionButton from '@/components/Shared/ActionButton.vue'
 
 export default {
-  name: "MainNav",
+  name: 'MainNav',
   components: {
     ActionButton,
     ProfileImage,
-    TheSubnav,
+    TheSubnav
   },
   data() {
     return {
       menuItems: [
-        "Teams",
-        "Locations",
-        "Life at Bobo Corp",
-        "How we hire",
-        "Students",
-        "Jobs",
+        { text: 'Teams', url: '/' },
+        { text: 'Locations', url: '/' },
+        { text: 'Life at Bobo Corp', url: '/' },
+        { text: 'How we hire', url: '/' },
+        { text: 'Students', url: '/' },
+        { text: 'Jobs', url: '/jobs/results' }
       ],
-      isLoggedIn: false,
-    };
+      isLoggedIn: false
+    }
   },
   computed: {
     headerHeightClass() {
       return {
-        "h-16": !this.isLoggedIn,
-        "h-32": this.isLoggedIn,
-      };
-    },
+        'h-16': !this.isLoggedIn,
+        'h-32': this.isLoggedIn
+      }
+    }
   },
   methods: {
     loginUser() {
-      this.isLoggedIn = true;
-    },
-  },
-};
+      this.isLoggedIn = true
+    }
+  }
+}
 </script>
