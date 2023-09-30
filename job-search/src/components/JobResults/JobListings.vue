c1d2ef2170ba4aabb9b9e64a1aa0ea54f6b2d55a
chaudhuree <chaudhuree00@gmail.com>
2023-09-30 09:57:35 +0600

    JobResultsView page template decoration done

---
+++ job-search/src/components/JobResults/JobListings.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="flex-auto bg-brand-gray-2 p-8">
+    <ol>
+      <job-listing />
+      <job-listing />
+      <job-listing />
+    </ol>
+  </main>
+</template>
+
+<script>
+import JobListing from "@/components/JobResults/JobListing.vue";
+
+export default {
+  name: "JobListings",
+  components: { JobListing },
+};
+</script>
