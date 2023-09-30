eb481f4fbacbbbb071965c2557dd8dc984ce966d
chaudhuree <chaudhuree00@gmail.com>
2023-09-30 06:27:31 +0600

    fixing the subnav dynamic view by returning this.$route.name === "JobResults"

--- job-search/src/components/Navigation/TheSubnav.vue
+++ job-search/src/components/Navigation/TheSubnav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
     <div class="flex h-full items-center px-8">
-      <div >
+      <div v-if="onJobResultsPage">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
         <span><span class="text-brand-green-1">1653</span> jobs matched</span>
       </div>
@@ -12,10 +12,10 @@
 <script>
 export default {
   name: "TheSubnav",
-  data() {
-    return {
-      onJobResultsPage: true,
-    };
+  computed: {
+    onJobResultsPage() {
+      return this.$route.name === "JobResults";
+    },
   },
 };
 </script>
