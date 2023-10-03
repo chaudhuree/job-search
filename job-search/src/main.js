b235ca8adb6c082c035f2210022d7691f2204eb4
chaudhuree <chaudhuree00@gmail.com>
2023-10-03 12:27:48 +0600

    installed pinia and register it in main.js

--- job-search/src/main.js
+++ job-search/src/main.js
@@ -1,15 +1,18 @@
-import { createApp } from "vue";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { createPinia } from "pinia";
+import { createApp } from "vue";
 
+import App from "@/App.vue";
 import "@/index.css";
 import router from "@/router";
-import App from "@/App.vue";
 
 library.add(faSearch);
 
+const pinia = createPinia();
 createApp(App)
+  .use(pinia)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
