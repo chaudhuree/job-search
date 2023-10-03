a118f5a743995b51c41551253ba0b4b2c5943422
chaudhuree <chaudhuree00@gmail.com>
2023-10-03 12:29:21 +0600

    impliment rendering state data and action in MainNav

--- job-search/src/components/Navigation/MainNav.vue
+++ job-search/src/components/Navigation/MainNav.vue
@@ -1,17 +1,27 @@
 <template>
   <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
-      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <router-link :to="{ name: 'Home' }" class="flex h-full items-center text-xl"
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <router-link
+          :to="{ name: 'Home' }"
+          class="flex h-full items-center text-xl"
           >Job Jam
         </router-link>
 
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
-            <li v-for="menuItem in menuItems" :key="menuItem.text" class="ml-9 h-full first:ml-0">
-              <router-link :to="menuItem.url" class="flex h-full items-center py-2.5">{{
-                menuItem.text
-              }}</router-link>
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem.text"
+              class="ml-9 h-full first:ml-0"
+            >
+              <router-link
+                :to="menuItem.url"
+                class="flex h-full items-center py-2.5"
+                >{{ menuItem.text }}</router-link
+              >
             </li>
           </ul>
         </nav>
@@ -28,42 +38,44 @@
 </template>
 
 <script>
-import ProfileImage from '@/components/Navigation/ProfileImage.vue'
-import TheSubnav from '@/components/Navigation/TheSubnav.vue'
-import ActionButton from '@/components/Shared/ActionButton.vue'
+import { mapActions, mapState } from "pinia";
+
+import { useUserStore } from "@/stores/user";
+
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import ProfileImage from "@/components/Navigation/ProfileImage.vue";
+import TheSubnav from "@/components/Navigation/TheSubnav.vue";
 
 export default {
-  name: 'MainNav',
+  name: "MainNav",
   components: {
     ActionButton,
     ProfileImage,
-    TheSubnav
+    TheSubnav,
   },
   data() {
     return {
       menuItems: [
-        { text: 'Teams', url: '/' },
-        { text: 'Locations', url: '/' },
-        { text: 'Life at Bobo Corp', url: '/' },
-        { text: 'How we hire', url: '/' },
-        { text: 'Students', url: '/' },
-        { text: 'Jobs', url: '/jobs/results' }
+        { text: "Teams", url: "/" },
+        { text: "Locations", url: "/" },
+        { text: "Life at Bobo Corp", url: "/" },
+        { text: "How we hire", url: "/" },
+        { text: "Students", url: "/" },
+        { text: "Jobs", url: "/jobs/results" },
       ],
-      isLoggedIn: false
-    }
+    };
   },
   computed: {
+    ...mapState(useUserStore, ["isLoggedIn"]),
     headerHeightClass() {
       return {
-        'h-16': !this.isLoggedIn,
-        'h-32': this.isLoggedIn
-      }
-    }
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn,
+      };
+    },
   },
   methods: {
-    loginUser() {
-      this.isLoggedIn = true
-    }
-  }
-}
+    ...mapActions(useUserStore, ["loginUser"]),
+  },
+};
 </script>
