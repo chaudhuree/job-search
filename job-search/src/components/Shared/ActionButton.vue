204de2dcae2edbd4ec46309a20c1e97701cd8366
chaudhuree <chaudhuree00@gmail.com>
2023-10-11 22:05:31 +0600

    transform: action button option api to composition api

--- job-search/src/components/Shared/ActionButton.vue
+++ job-search/src/components/Shared/ActionButton.vue
@@ -4,40 +4,42 @@
   </button>
 </template>
 
-<script>
-export default {
-  name: 'ActionButton',
-  props: {
-    text: {
-      type: String,
-      required: true
-    },
-    type: {
-      type: String,
-      required: false,
-      default: 'primary',
-      validator(value) {
-        return ['primary', 'secondary'].includes(value)
-      }
-    }
+<script setup>
+import { computed, toRefs } from "vue";
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
   },
-  computed: {
-    buttonClass() {
-      return {
-        [this.type]: true
-      }
-    }
-  }
-}
+  type: {
+    type: String,
+    required: false,
+    default: "primary",
+    validator(value) {
+      return ["primary", "secondary"].includes(value);
+    },
+  },
+});
+
+const { type } = toRefs(props);
+
+const buttonClass = computed(() => {
+  return {
+    [type.value]: true,
+  };
+});
 </script>
 
 <style scoped>
 button {
   @apply px-5 py-3 font-medium;
 }
+
 .primary {
-  @apply bg-brand-blue-1 rounded text-white hover:shadow-blue;
+  @apply rounded bg-brand-blue-1 text-white hover:shadow-blue;
 }
+
 .secondary {
   @apply bg-transparent text-brand-blue-1 hover:bg-brand-blue-2 hover:text-white;
 }
