eaf333fa36caff79300e0efc7333f88740c5ee32
chaudhuree <chaudhuree00@gmail.com>
2023-10-12 11:21:04 +0600

    added the vue 2 code in collapsibleaccordion and actionbutton for furhter reference

--- job-search/src/components/Shared/ActionButton.vue
+++ job-search/src/components/Shared/ActionButton.vue
@@ -44,3 +44,32 @@ button {
   @apply bg-transparent text-brand-blue-1 hover:bg-brand-blue-2 hover:text-white;
 }
 </style>
+
+
+<!-- vue-2 style coding -->
+<!-- <script>
+export default {
+  name: 'ActionButton',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'primary',
+      validator(value) {
+        return ['primary', 'secondary'].includes(value)
+      }
+    }
+  },
+  computed: {
+    buttonClass() {
+      return {
+        [this.type]: true
+      }
+    }
+  }
+}
+</script> -->
--- job-search/src/components/Shared/CollapsibleAccordion.vue
+++ job-search/src/components/Shared/CollapsibleAccordion.vue
@@ -38,3 +38,36 @@ const caretIcon = computed(() =>
   isOpen.value ? ["fas", "angle-up"] : ["fas", "angle-down"]
 );
 </script>
+
+
+<!-- 
+
+<script>
+export default {
+  name: "CollapsibleAccordion",
+  props: {
+    header: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  computed: {
+    caretIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
+
+
+ -->
