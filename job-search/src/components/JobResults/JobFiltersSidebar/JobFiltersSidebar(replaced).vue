45f3c3db2710dc5436c27dd6ce3595895ce588cd
chaudhuree <chaudhuree00@gmail.com>
2023-10-13 12:20:37 +0600

    created: reusable sidenav and for the collapsable item new item introduced jobfilterssidebarcheckboxgroup

---
+++ job-search/src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar(replaced).vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
+  >
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+
+      <collapsible-accordion header="Degree" />
+
+      <job-filters-sidebar-job-types />
+
+      <job-filters-sidebar-organizations />
+    </section>
+  </div>
+</template>
+
+<script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+import JobFiltersSidebarJobTypes from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue";
+import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
+
+export default {
+  name: "JobFiltersSidebar",
+  components: {
+    ActionButton,
+    CollapsibleAccordion,
+    JobFiltersSidebarJobTypes,
+    JobFiltersSidebarOrganizations,
+  },
+};
+</script>
